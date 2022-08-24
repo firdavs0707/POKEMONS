943ecb340f7c0f019350a5809c5443e2c711d1f9
firdavs0707 <firdavs0.4.2.0.0.7>
2022-08-24 17:06:28 +0500

    add codes

--- js/script.js
+++ js/script.js
@@ -10,18 +10,23 @@ pokemons.forEach(pokemon =>{
   let title = CreateElement('h3');
   let text = CreateElement('p');
   let cardBody = CreateElement('div')
+
   title.textContent = pokemon.name
   text.textContent = pokemon.weaknesses.join(', ')
   img.src = pokemon.img;
   img.alt = pokemon.name;
+
   elList.appendChild(li)
   cardBody.append(title, text)
+
   img.classList.add('card-img-top', 'd-block', 'mx-auto')
   title.classList.add('card-title', 'text-center')
   text.classList.add('card-text', 'text-center')
   cardBody.classList.add('card-body')
-  for (let i = 0; i < pokemons.length; i++) {
-    li.append(img, cardBody)
-    li.classList.add('card', 'm-2', 'width','text-light')
-  }
+
+  
+  li.append(img, cardBody)
+  li.classList.add('card', 'm-2', 'width','text-light')
+  
+
 })
